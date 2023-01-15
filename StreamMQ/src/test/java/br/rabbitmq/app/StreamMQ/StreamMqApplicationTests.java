ea9f07547bd096a860c2de7f1d0ef04b5b731361
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-01-15 18:05:31 -0300

    Streams com RabbitMQ

---
+++ StreamMQ/src/test/java/br/rabbitmq/app/StreamMQ/StreamMqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rabbitmq.app.StreamMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
